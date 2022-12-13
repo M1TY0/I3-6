b52f131cf99e7c2eeeaf99faee1da749bad496ff
M1TY0 <100305309+M1TY0@users.noreply.github.com>
2022-12-13 16:13:59 +0200

    done*

--- I3-6/Program.cs
+++ I3-6/Program.cs
@@ -1,19 +1,20 @@
 ﻿int numOfLInes = int.Parse(Console.ReadLine());
-double[] num1 = new double[numOfLInes*2+2];
+double[] num = new double[numOfLInes * 2 + 2];
 double lenght = 0;
 for (int i = 0; i < numOfLInes * 2 + 2; i++)
 {
-    num1[i] = double.Parse(Console.ReadLine());
+    num[i] = double.Parse(Console.ReadLine());
 }
-for (int i = 0; i < numOfLInes; i++)
+for (int i = 0; i < numOfLInes*2; i+=2)
 {
-    lenght += Lenght(num1[i], num1[i] + 1, num1[i] + 2, num1[i] + 3);
+    lenght += Lenght(num[i], num[i+1] , num[i+2] , num[i+3]);
 }
-Console.WriteLine(  lenght);
-
+Console.WriteLine(lenght);
+double lastl = Lenght(num[0], num[1], num[numOfLInes * 2 ], num[numOfLInes * 2 + 1]);
+Console.WriteLine(lenght+lastl);
 double Lenght(double n = 0, double n1 = 0, double n2 = 0, double n3 = 0)
 {
-    double num = (Math.Pow((n - n1), 2) + Math.Pow((n2 - n3), 2));
+    double num = (Math.Pow((n2 - n), 2) + Math.Pow((n3 - n1), 2));
     int num2 = (int)num;
     return Math.Sqrt(num2);
 
